05b4eff4f922ca957e0099771ab98c3dc04ffc81
abdushukur771 <abdushukur771@mail.ru>
2022-01-17 01:17:02 +0500

    updated

---
+++ 9.oop4(modul).py
@@ -0,0 +1,8 @@
+from avto_modul import Avto
+
+avto1 = Avto("GM", "Malibu", "Qora", 2020, 40000, 100000)
+print(Avto.get_num_avto())
+avto2 = Avto("GM", "Lacetti", "Oq", 2020, 20000)
+print(Avto.get_num_avto())
+avto3 = Avto("Toyota", "Camry", "Silver", 2018, 45000)
+print(Avto.get_num_avto())
